a4db3e48682e005a8310dea1a67cc21b48654f8a
VadimGodovikov <119358867+VadimGodovikov@users.noreply.github.com>
2022-12-21 18:13:53 +0300

    git add

---
+++ prog11/prog11/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog10._1
+{
+	class RegularEx
+	{
+
+		public RegularEx(string pattern, string txt)
+		{
+			r = new Regex(pattern.ToLower());
+			text = txt;
+		}
+
+		// поля
+
+		private Regex r;
+		private string text;
+
+		public bool Exist() // содержит ли текст фрагменты, соответствующие шаблону поля;
+		{
+			if (r.IsMatch(text.ToLower()))
+			{
+				Console.WriteLine("Содержит");
+				return r.IsMatch(text);
+			}
+			else
+			{
+				Console.WriteLine("Не содержит");
+				return false;
+			}
+		}
+
+		public void ShowMatches() // •	вывести на экран все фрагменты текста, соответствующие шаблону поля;
+		{
+			MatchCollection m = r.Matches(text.ToLower());
+			foreach (Match x in m)
+			{
+				Console.Write(x.Value + " ");
+			}
+
+		}
+
+		public string DeleteMatches() // •	удалить из текста все фрагменты, соответствующие шаблону поля;
+		{
+			MatchCollection m = r.Matches(text.ToLower());
+			string s = text.ToLower();
+			foreach (Match x in m)
+			{
+				int i = s.IndexOf(x.Value);
+				int l = x.Value.Length;
+				s = s.Remove(i, l);
+				text = text.Remove(i, l);
+				
+			}
+			Console.WriteLine(text);
+			return text;
+		}
+
+		public Regex R // •	св-во позволяющее установить или получить регулярное выражение, хранящееся в соответствующем поле класса (доступно для чтения и записи)
+		{
+			get { return r; }
+			set { r = value; }
+		}
+
+		public string Text // •	св-во позволяющее установить или получить строковое поле класса (доступно для чтения и записи)
+		{
+			get { return text; }
+			set { text = value; }
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				txt1: Console.WriteLine("Введите любой текст как с символами/словами, так и с цифрами: ");
+				string text = Console.ReadLine();
+				if(text == string.Empty)
+				{
+					Console.WriteLine("Ошибка ввода данных");
+					goto txt1;
+				}
+				Console.Write("Введите шаблон поля регулярного выражения: ");
+				string rex = Console.ReadLine();
+
+				RegularEx A = new RegularEx(rex, text);
+				Console.WriteLine("Содержит ли текс шаблон: ");
+				A.Exist();
+				Console.WriteLine("Вывод шаблона: ");
+				A.ShowMatches();
+				Console.WriteLine("\nУдаление из текста все фрагменты, соответствующие шаблону поля");
+				Console.WriteLine("Исходный текст: ");
+				A.Text = A.DeleteMatches();
+				Console.WriteLine("Введите новую строку: ");
+				A.Text = Console.ReadLine();
+				Console.WriteLine("Введите новый шаблон: ");
+				string rx = Console.ReadLine();
+				Regex reg = new Regex(rx);
+				A.R = reg;
+				Console.WriteLine("Содержит ли текс шаблон: ");
+				A.Exist();
+				Console.WriteLine("Вывод шаблона: ");
+				A.ShowMatches();
+				Console.WriteLine("\nУдаление из текста все фрагменты, соответствующие шаблону поля");
+				Console.WriteLine("Исходный текст: ");
+				A.Text = A.DeleteMatches();
+
+				Console.WriteLine();
+			}
+			catch
+			{
+				Console.WriteLine("Ошибка работы программы");
+				return;
+			}
+
+		}
+	}
+}
---
+++ prog11form/prog11form/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prog11form
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		class RegularEx
+		{
+
+			public RegularEx(string pattern, string txt)
+			{
+				r = new Regex(pattern.ToLower());
+				text = txt;
+			}
+
+			// поля
+
+			private Regex r;
+			private string text;
+
+			public bool Exist(TextBox textBox) // содержит ли текст фрагменты, соответствующие шаблону поля;
+			{
+				if (r.IsMatch(text.ToLower()))
+				{
+					textBox.Text += "Содержит" + Environment.NewLine;
+					return r.IsMatch(text);
+				}
+				else
+				{
+					textBox.Text += "Не Содержит" + Environment.NewLine;
+					return false;
+				}
+			}
+
+			public void ShowMatches(TextBox textBox) // •	вывести на экран все фрагменты текста, соответствующие шаблону поля;
+			{
+				MatchCollection m = r.Matches(text.ToLower());
+				foreach (Match x in m)
+				{
+					textBox.Text += $"{x.Value} ";
+				}
+
+			}
+
+			public string DeleteMatches(TextBox textBox) // •	удалить из текста все фрагменты, соответствующие шаблону поля;
+			{
+				MatchCollection m = r.Matches(text.ToLower());
+				string s = text.ToLower();
+				foreach (Match x in m)
+				{
+					int i = s.IndexOf(x.Value);
+					int l = x.Value.Length;
+					s = s.Remove(i, l);
+					text = text.Remove(i, l);
+
+				}
+				textBox.Text += s;
+				return s;
+			}
+
+			public Regex R // •	св-во позволяющее установить или получить регулярное выражение, хранящееся в соответствующем поле класса (доступно для чтения и записи)
+			{
+				get { return r; }
+				set { r = value; }
+			}
+
+			public string Text // •	св-во позволяющее установить или получить строковое поле класса (доступно для чтения и записи)
+			{
+				get { return text; }
+				set { text = value; }
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			resultBox.Text = "Результат работы программы: " + Environment.NewLine;
+			try
+			{
+				string text = textBox1.Text;
+				if (text == string.Empty)
+				{
+					MessageBox.Show("Ошибка: строка не должна быть пустая");
+					return;
+				}
+				string rex = textBox2.Text;
+				RegularEx A = new RegularEx(rex, text);
+				Regex reg = new Regex(rex);
+				A.Text = text;
+				A.R = reg;
+				resultBox.Text += "Содержит ли текст шаблон: " + Environment.NewLine;
+				A.Exist(resultBox);
+				resultBox.Text += "Вывод текста, который содержит в себе шаблон: " + Environment.NewLine;
+				A.ShowMatches(resultBox);
+				resultBox.Text += "\r\nУдаление из текста все фрагменты, соответствующие шаблону поля" + Environment.NewLine;
+				resultBox.Text += "Исходный текст: " + Environment.NewLine;
+				A.Text = A.DeleteMatches(resultBox);
+
+			}
+			catch
+			{
+				Console.WriteLine("Ошибка работы программы");
+				return;
+			}
+		}
+	}
+}
